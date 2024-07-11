76cf45aae3df457e6d688356a045dffa31baf166
ejamescodes <47791697+ejamescodes@users.noreply.github.com>
2024-07-10 23:16:50 -0400

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let n = parseInt(a);
+    let fib = [0, 1];
+    if(n > 1){
+        for(let i = 1; i <= n; i++){
+            fib.push(fib[fib.length-1]+fib[fib.length-2]);
+        }
+    } else if(n < 0){
+        return "OOPS"
+    }
+    return fib[n];
 };
 
 // Do not edit below this line
