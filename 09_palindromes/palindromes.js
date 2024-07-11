7254b03efbf046e1b9f8d381382cc45871dccc4b
ejamescodes <47791697+ejamescodes@users.noreply.github.com>
2024-07-10 22:44:55 -0400

    complete 09_palindromes git commit -m complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const alphanum = "1234567890qwertyuiopasdfghjklzxcvbnm"
+    const letters = phrase
+                    .toLowerCase()
+                    .split('')
+                    .filter((letter) => alphanum.includes(letter));
+    const phraseFW = letters.join('');
+    const phraseRV = letters.reverse().join('');
+    return phraseFW === phraseRV;
 };
 
 // Do not edit below this line
