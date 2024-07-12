cf81e98688a4afe3984313b7491b90daea5b998a
ejamescodes <47791697+ejamescodes@users.noreply.github.com>
2024-07-11 23:07:06 -0400

    work on findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people
+                    .filter((person) => {
+                        'yearOfBirth' in person && 'yearOfDeath' in person;
+                    })
+                    .sort((a, b) => {
+                        let ageA = a.yearOfDeath-a.yearOfBirth;
+                        let ageB = b.yearOfDeath-b.yearOfBirth;
+                        if(ageA > ageB){
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    });
+    const oldest = ages[0];
+    return oldest;
 };
 
 // Do not edit below this line
